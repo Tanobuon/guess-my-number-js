29d1f4adbc26e4cb6018e7153a155debc6cbb2e7
TanoBuon <tano.buon.it@hotmail.com>
2023-02-28 22:17:55 +1100

    refactoring

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let score = 20;
+let highScore = 0;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+//
+
+//function for the message query selector
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+//function for the click button
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //when there is no input
+  if (!guess) {
+    displayMessage('❌ No Number inserted!');
+
+    //when players wins
+  } else if (guess === secretNumber) {
+    displayMessage('✅ Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too low');
+      //document.querySelector('.message').textContent =
+      //guess > secretNumber ? '📈 Too High!' : '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+//function for the Again Button
+document.querySelector('.again').addEventListener('click', function () {
+  //reset parameters
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').style.width = '15rem';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+});
